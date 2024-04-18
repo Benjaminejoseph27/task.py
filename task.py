658751b2428d6c8087017c0829c277c96d2d5c2b
Benjaminejoseph27 <benjaminejoseph2707@gmail>
2024-04-18 12:13:24 +0530

    introduction

---
+++ task.py
@@ -0,0 +1,45 @@
+
+##Exploring Basic Datatypes
+"""
+print("hello world")
+print('hello world')
+print('''hello
+everyone''')
+print(123,56,76)
+print(123.45)
+print(123.45j)
+print(123.45j)
+print(True)
+print(False)
+##print(5/0)
+"""
+
+##How to check datatypes?
+
+##type()
+
+##print(type("Hello"))
+##print(type("4565"))
+##print(type("HELLO"))
+##print(type(465))
+##print(type(465.55))
+##print(type(465j))
+##print(type(True))
+##print(type(False))
+
+##What is Variables?
+
+a =[10]
+b =[10]
+c =10
+d =10
+print(type(a))
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+print()
+print(a is b)
+print(a == b)
+print(c is d)
+print(c == d)
